75592b79c6ef69bdbb69a1850720f65cd47f42b8
Allef Sousa <allefsousa_1@hotmail.com>
2020-02-15 17:18:19 -0300

    Criando arquivo de customiação css da view

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React,{ useState } from 'react';
-import Header from './Header';
+import React from 'react';
+import './global.css';
 
-//Componet = Bloco isolado de HTML, CSS e JS 
-// Propriedade = Informações que um componenet pai passa para um filho
-// Estado = Informações mantidas pelo componenete
+
 function App() {
-  const [counter,setCounter] =useState(0);
-
-  function inclementCounter(){
-    setCounter(counter + 1)
-  }
-  
   return (
-    <>
-    <h1>Contador: {counter}</h1>
-    <button onClick={inclementCounter}>Incrementar</button>
-  </>
+    <div id="app">
+      <aside>
+
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
