5347ba87aca2f4edc70e07f2937ec127b6e0ac40
Allef Sousa <allefsousa_1@hotmail.com>
2020-02-15 22:36:27 -0300

    Criando formulario de cadastro

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import './global.css';
+import './App.css';
+import './Sidebar.css';
+
 
 
 function App() {
   return (
     <div id="app">
       <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
 
+             <label htmlFor="geithub_username">Usuario do Github</label>
+              <input name="geithub_username" id="geithub_username" required/>
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="techs">Tecnologias</label>
+              <input name="techs" id="techs" required/>
+            </div>
+
+            <div className="input-group">
+
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required/>
+            </div>
+
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required/>
+            </div>
+            </div>
+            <button type="submit">Salvar</button>
+
+        </form>
       </aside>
       <main>
+        <ul>
+          
+        </ul>
 
       </main>
     </div>
